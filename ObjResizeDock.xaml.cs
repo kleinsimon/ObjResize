19d3ac0034310e84fe1828e1b3298fbf94423684
Simon Klein <simon.klein@outlook.de>
2017-10-30 16:18:31 +0100

    init

---
+++ ObjResizeDock.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CorelDRAW;
+
+
+namespace ObjResizer
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class ObjResizeDock : UserControl
+    {
+        CorelDRAW.Application CDWin;
+
+        public ObjResizeDock()
+        {
+            InitializeComponent();
+        }
+
+        public ObjResizeDock(object app)
+        {
+            InitializeComponent();
+            CDWin = (CorelDRAW.Application)app;
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            startResize();
+        }
+
+        private void startResize()
+        {
+            CDWin.Application.Optimization = true;
+            CDWin.ActiveDocument.BeginCommandGroup("Resize-Group");
+
+            try
+            {
+                doResize();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            CDWin.ActiveWindow.Refresh();
+            CDWin.Application.Refresh();
+
+            CDWin.ActiveDocument.EndCommandGroup();
+            CDWin.Application.Optimization = false;
+        }
+
+        private void doResize()
+        {
+            CorelDRAW.cdrReferencePoint refPoint = posSel.referencePoint;
+
+            double scaleH = 1d;
+            double scaleV = 1d;
+
+            if (!double.TryParse(newWidthValue.Text, out scaleH) || !double.TryParse(newHeightValue.Text, out scaleV))
+            {
+                return;
+            }
+
+            scaleH = scaleH / 100;
+            scaleV = scaleV / 100;
+
+            foreach (Shape sh in CDWin.ActiveSelection.Shapes)
+            {
+                double ow = sh.SizeWidth;
+                double oh = sh.SizeHeight;
+
+                double nw = ow * scaleH;
+                double nh = oh * scaleV;
+
+                double ox = 0d;
+                double oy = 0d;
+
+                sh.GetPositionEx(refPoint, out ox, out oy);
+                sh.SetSize(nw, nh);
+                sh.SetPositionEx(refPoint, ox, oy);
+
+            }
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            about tmp = new about();
+            tmp.ShowDialog();
+        }
+    }
+}
---
+++ positionSelector.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObjResizer
+{
+    /// <summary>
+    /// Interaktionslogik für positionSelector.xaml
+    /// </summary>
+    public partial class positionSelector : UserControl
+    {
+        Dictionary<CorelDRAW.cdrReferencePoint, UIElement> rbAssign = new Dictionary<CorelDRAW.cdrReferencePoint, UIElement>();
+
+        public CorelDRAW.cdrReferencePoint referencePoint
+        {
+            get
+            {
+                var sel = getSelected();
+                return rbAssign.FirstOrDefault(x => x.Value == sel).Key;
+            }
+            set
+            {
+                setView(value);
+            }
+        }
+
+        public positionSelector()
+        {
+            InitializeComponent();
+
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrBottomLeft, rb_bl);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrBottomMiddle, rb_bc);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrBottomRight, rb_br);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrCenter, rb_mc);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrMiddleLeft, rb_ml);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrMiddleRight, rb_mr);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrTopLeft, rb_tl);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrTopMiddle, rb_tc);
+            rbAssign.Add(CorelDRAW.cdrReferencePoint.cdrTopRight, rb_tr);
+        }
+
+        private void setView(CorelDRAW.cdrReferencePoint refPos)
+        {
+            ((RadioButton)rbAssign[refPos]).IsChecked = true;
+        }
+
+        private UIElement getSelected()
+        {
+            foreach (RadioButton rb in rbGrid.Children)
+            {
+                if (rb.IsChecked == true)
+                {
+                    return rb;
+                }
+            }
+            return rb_mc;
+        }
+    }
+}
